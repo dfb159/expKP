dacbfd613fba9e38f8216f26dafa8a648914ea5f
Jatiza <42943494+Jatiza@users.noreply.github.com>
2019-12-01 21:29:56 +0100

    Auswertung nearly done

--- V13-NachweisVonMyonen/src/AuswertungLebensdauerWinkel.py
+++ V13-NachweisVonMyonen/src/AuswertungLebensdauerWinkel.py
@@ -16,10 +16,7 @@ Counts75Deg = np.loadtxt("landau_75_working.txt", dtype=int, skiprows=12, usecol
 # Initialize global variables:
 n = len(pulse)
 channel = np.linspace(0, n-1, num=n)
-ActiveMeasurementTime0Deg = 78561.
-ActiveMeasurementTime30Deg = 88443.
-ActiveMeasurementTime45Deg = 91368.
-ActiveMeasurementTime75Deg = 260489.
+ActiveMeasurementTime = [78561., 88443., 91368., 260489.]
 
 
 # Some functions to fit and calculate:
@@ -133,8 +130,7 @@ output1 = ODR1.run()
 
 
 #------------------------------------------------------------------------------
-
-
+# Energy loss and Landau distribution:
 # For the 0 degrees spectrum:
 # Bin the channels and their counts:
 BinnedCounts0Deg = np.zeros(n)
@@ -151,12 +147,13 @@ for i in range(0, n, 4):
 
 # Create a Landau fit for the energy spectrum; because of background start at the array element 400:
 model2 = odr.Model(LandauDistribution)
-data2 = odr.RealData(channel[400:], BinnedCounts0Deg[400:]/ActiveMeasurementTime0Deg, sx=np.repeat(0.5/np.sqrt(3), int(n - 400)), sy=np.divide(list(map(OneOrGreater, BinnedCounts0DegUnc)), ActiveMeasurementTime0Deg)[400:])
+data2 = odr.RealData(channel[400:], BinnedCounts0Deg[400:]/ActiveMeasurementTime[0], sx=np.repeat(0.5/np.sqrt(3), int(n - 400)), sy=np.divide(list(map(OneOrGreater, BinnedCounts0DegUnc)), ActiveMeasurementTime[0])[400:])
 ODR2 = odr.ODR(data2, model2, beta0=[0.002, 500., 250.])
 output2 = ODR2.run()
 
 
 # For the 30 degrees spectrum:
+# Bin the channels and their counts:
 BinnedCounts30Deg = np.zeros(n)
 BinnedCounts30DegUnc = np.zeros(n)
 for i in range(0, n, 4):
@@ -171,9 +168,59 @@ for i in range(0, n, 4):
 
 # Create a Landau fit for the energy spectrum; because of background start at the array element 400:
 model3 = odr.Model(LandauDistribution)
-data3 = odr.RealData(channel[400:], BinnedCounts30Deg[400:]/ActiveMeasurementTime30Deg, sx=np.repeat(0.5/np.sqrt(3), int(n - 400)), sy=np.divide(list(map(OneOrGreater, BinnedCounts30DegUnc)), ActiveMeasurementTime30Deg)[400:])
+data3 = odr.RealData(channel[400:], BinnedCounts30Deg[400:]/ActiveMeasurementTime[1], sx=np.repeat(0.5/np.sqrt(3), int(n - 400)), sy=np.divide(list(map(OneOrGreater, BinnedCounts30DegUnc)), ActiveMeasurementTime[1])[400:])
 ODR3 = odr.ODR(data3, model3, beta0=[0.01, 500., 50.])
 output3 = ODR3.run()
+
+
+# For the 45 degrees spectrum:
+# Bin the channels and their counts:
+BinnedCounts45Deg = np.zeros(n)
+BinnedCounts45DegUnc = np.zeros(n)
+for i in range(0, n, 4):
+    BinnedCounts45Deg[i] = .25 * (Counts45Deg[i] + Counts45Deg[i+1] + Counts45Deg[i+2] + Counts45Deg[i+3])
+    BinnedCounts45Deg[i+1] = BinnedCounts45Deg[i]
+    BinnedCounts45Deg[i+2] = BinnedCounts45Deg[i]
+    BinnedCounts45Deg[i+3] = BinnedCounts45Deg[i]
+    BinnedCounts45DegUnc[i] = .25 * np.sqrt(Counts45Deg[i] + Counts45Deg[i+1] + Counts45Deg[i+2] + Counts45Deg[i+3])
+    BinnedCounts45DegUnc[i+1] = BinnedCounts45DegUnc[i]
+    BinnedCounts45DegUnc[i+2] = BinnedCounts45DegUnc[i]
+    BinnedCounts45DegUnc[i+3] = BinnedCounts45DegUnc[i]
+
+# Create a Landau fit for the energy spectrum; because of background start at the array element 400:
+model4 = odr.Model(LandauDistribution)
+data4 = odr.RealData(channel[400:], BinnedCounts45Deg[400:]/ActiveMeasurementTime[2], sx=np.repeat(0.5/np.sqrt(3), int(n - 400)), sy=np.divide(list(map(OneOrGreater, BinnedCounts45DegUnc)), ActiveMeasurementTime[2])[400:])
+ODR4 = odr.ODR(data4, model4, beta0=[0.01, 500., 50.])
+output4 = ODR4.run()
+
+
+# For the 75 degrees spectrum:
+# Bin the channels and their counts:
+BinnedCounts75Deg = np.zeros(n)
+BinnedCounts75DegUnc = np.zeros(n)
+for i in range(0, n, 4):
+    BinnedCounts75Deg[i] = .25 * (Counts75Deg[i] + Counts75Deg[i+1] + Counts75Deg[i+2] + Counts75Deg[i+3])
+    BinnedCounts75Deg[i+1] = BinnedCounts75Deg[i]
+    BinnedCounts75Deg[i+2] = BinnedCounts75Deg[i]
+    BinnedCounts75Deg[i+3] = BinnedCounts75Deg[i]
+    BinnedCounts75DegUnc[i] = .25 * np.sqrt(Counts75Deg[i] + Counts75Deg[i+1] + Counts75Deg[i+2] + Counts75Deg[i+3])
+    BinnedCounts75DegUnc[i+1] = BinnedCounts75DegUnc[i]
+    BinnedCounts75DegUnc[i+2] = BinnedCounts75DegUnc[i]
+    BinnedCounts75DegUnc[i+3] = BinnedCounts75DegUnc[i]
+
+# Create a Landau fit for the energy spectrum; because of background start at the array element 450:
+model5 = odr.Model(LandauDistribution)
+data5 = odr.RealData(channel[450:800], BinnedCounts75Deg[450:800]/ActiveMeasurementTime[3], sx=np.repeat(0.5/np.sqrt(3), int(800 - 450)), sy=np.divide(list(map(OneOrGreater, BinnedCounts75DegUnc)), ActiveMeasurementTime[3])[450:800])
+ODR5 = odr.ODR(data5, model5, beta0=[0.001, 500., 55.])
+output5 = ODR5.run()
+
+
+# Calculate the count rates:
+print(sum(Counts0Deg)/ActiveMeasurementTime[0])
+print(sum(Counts30Deg)/ActiveMeasurementTime[1])
+print(sum(Counts45Deg)/ActiveMeasurementTime[2])
+print(sum(Counts75Deg)/ActiveMeasurementTime[3])
+#CountRates = []
 
 
 #------------------------------------------------------------------------------
@@ -211,10 +258,13 @@ output3 = ODR3.run()
 #print(output1.beta)
 #print(output1.sd_beta)
 
+
+#------------------------------------------------------------------------------
+# Show results:
 # Energy spectrum at 0 degrees:
 plt.figure("Energy spectrum at 0 degrees", figsize=(10, 7.5))
 plt.axvline(x=channel[399], color="C1", linestyle="--", linewidth=2.5, zorder=4)
-plt.errorbar(channel, BinnedCounts0Deg/ActiveMeasurementTime0Deg, xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts0DegUnc/ActiveMeasurementTime0Deg, capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
+plt.errorbar(channel, BinnedCounts0Deg/ActiveMeasurementTime[0], xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts0DegUnc/ActiveMeasurementTime[0], capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
 plt.plot(channel, LandauDistribution(output2.beta, channel), "-r", linewidth=3.5, zorder=3, label="Landau fit")
 plt.legend(loc="best", fontsize=18)
 plt.xlabel(r"Channel $K$", fontsize=20)
@@ -232,7 +282,7 @@ print(output2.sd_beta)
 # Energy spectrum at 30 degrees:
 plt.figure("Energy spectrum at 30 degrees", figsize=(10, 7.5))
 plt.axvline(x=channel[399], color="C1", linestyle="--", linewidth=2.5, zorder=4)
-plt.errorbar(channel, BinnedCounts30Deg/ActiveMeasurementTime30Deg, xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts30DegUnc/ActiveMeasurementTime30Deg, capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
+plt.errorbar(channel, BinnedCounts30Deg/ActiveMeasurementTime[1], xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts30DegUnc/ActiveMeasurementTime[1], capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
 plt.plot(channel, LandauDistribution(output3.beta, channel), "-r", linewidth=3.5, zorder=3, label="Landau fit")
 plt.legend(loc="best", fontsize=18)
 plt.xlabel(r"Channel $K$", fontsize=20)
@@ -246,3 +296,45 @@ plt.show()
 
 print(output3.beta)
 print(output3.sd_beta)
+
+# Energy spectrum at 45 degrees:
+plt.figure("Energy spectrum at 45 degrees", figsize=(10, 7.5))
+plt.axvline(x=channel[399], color="C1", linestyle="--", linewidth=2.5, zorder=4)
+plt.errorbar(channel, BinnedCounts45Deg/ActiveMeasurementTime[2], xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts45DegUnc/ActiveMeasurementTime[2], capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
+plt.plot(channel, LandauDistribution(output4.beta, channel), "-r", linewidth=3.5, zorder=3, label="Landau fit")
+plt.legend(loc="best", fontsize=18)
+plt.xlabel(r"Channel $K$", fontsize=20)
+plt.ylabel(r"Counts $N$", fontsize=20)
+plt.xlim(0., 1500.)
+plt.xticks(np.linspace(0., 1500., 7, endpoint=True))
+plt.tick_params(labelsize=16)
+plt.grid(True)
+#plt.savefig("EnergySpectrumAt45Degrees.pdf")
+plt.show()
+
+print(output4.beta)
+print(output4.sd_beta)
+
+# Energy spectrum at 75 degrees:
+plt.figure("Energy spectrum at 75 degrees", figsize=(10, 7.5))
+plt.axvline(x=channel[449], color="C1", linestyle="--", linewidth=2.5, zorder=4)
+plt.errorbar(channel, BinnedCounts75Deg/ActiveMeasurementTime[3], xerr=np.repeat(0.5/np.sqrt(3), n), yerr=BinnedCounts75DegUnc/ActiveMeasurementTime[3], capsize=5., ecolor="C0", fmt=".C0", zorder=2, label="Binned data points")
+plt.plot(channel, LandauDistribution(output5.beta, channel), "-r", linewidth=3.5, zorder=3, label="Landau fit")
+plt.legend(loc="best", fontsize=18)
+plt.xlabel(r"Channel $K$", fontsize=20)
+plt.ylabel(r"Counts $N$", fontsize=20)
+plt.xlim(0., 1500.)
+plt.xticks(np.linspace(0., 1500., 7, endpoint=True))
+plt.tick_params(labelsize=16)
+plt.grid(True)
+#plt.savefig("EnergySpectrumAt75Degrees.pdf")
+plt.show()
+
+print(output5.beta)
+print(output5.sd_beta)
+
+#plt.figure("test", figsize=(10, 7.5))
+#plt.plot(channel[425:], BinnedCounts75Deg[425:]/ActiveMeasurementTime[3], ".k")
+#plt.xlim(0., 1500.)
+#plt.grid(True)
+#plt.show()
